1fbb5ea161e00e4999a3ac240619e2fac6a0881f
deconey87 <deconey87@gmail.com>
2024-08-28 18:31:34 +0900

    feat: 🎸 11_hashmaps

--- exercises/quizzes/quiz2.rs
+++ exercises/quizzes/quiz2.rs
@@ -27,7 +27,24 @@ mod my_module {
     use super::Command;
 
     // TODO: Complete the function as described above.
-    // pub fn transformer(input: ???) -> ??? { ??? }
+    pub fn transformer(input: Vec<(String, Command)>) -> Vec<String> {
+        let mut result: Vec<String> = vec![]; // Vec::new() でも可
+        for (string, command) in input {
+            let modified = match command {
+                Command::Uppercase => string.to_uppercase(),
+                Command::Trim => string.trim().to_string(),
+                Command::Append(size) => {
+                    let mut res = string.clone();
+                    for _i in 0..size {
+                        res += "bar"
+                    }
+                    res
+                } // string + &"bar".repeat(n) とも書ける
+            };
+            result.push(modified);
+        }
+        result
+    }
 }
 
 fn main() {
@@ -37,7 +54,7 @@ fn main() {
 #[cfg(test)]
 mod tests {
     // TODO: What do we need to import to have `transformer` in scope?
-    // use ???;
+    use super::my_module::transformer;
     use super::Command;
 
     #[test]
